1dba74cb669db2f3af42e3b936d18023696b46dd
sacdev7 <82803256+sacdev7@users.noreply.github.com>
2021-04-19 13:30:36 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,46 @@
+var boxx = document.getElementById("box");
+
+var boxxa = document.getElementById("boxa");
+
+var boxxb = document.getElementById("boxb");
+
+var boxxc = document.getElementById("boxc");
+
+var boxxd = document.getElementById("boxd");
+
+var boxxe = document.getElementById("boxe");
+
+var boxxf = document.getElementById("boxf");
+
+var boxxg = document.getElementById("boxg");
+
+var boxxh = document.getElementById("boxh");
+
+var boxxi = document.getElementById("boxi");
+
+function numgen() {
+
+  boxx.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxa.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxb.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxc.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxd.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxe.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxf.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxg.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxh.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ boxxi.innerHTML = Math.floor(Math.random() * 9) + 1; ;
+
+ 
+
+}
+
